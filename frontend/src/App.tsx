66c60c3344caa031d67419c819fecaf6a4d06329
ThomasNVu <139417064+ThomasNVu@users.noreply.github.com>
2025-10-16 17:50:45 -0400

    Revert "Merge branch 'main' into feature/calendar-styles"
    This reverts commit 416377c0e43e5999ad18b9db9eb448e4e95e5769, reversing
    changes made to ae3bacbd9ee61c9ebf257ba9d8f3b82f72da76e3.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
 import Navbar from "./component/Navbar";
 import Calendar from "./component/Calendar";
-import Login from "./pages/Login"; // Import the Login component
 
 function App() {
   return (
     <div>
       <Navbar />
       <Calendar />
-      <Login /> {/* Render the Login component here */}
     </div>
   );
 }
