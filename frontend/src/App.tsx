856d0d458fb0b7ad411e21c80544addfdaf53bc3
ThomasNVu <139417064+ThomasNVu@users.noreply.github.com>
2025-10-16 17:51:41 -0400

    Correct routing to App file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import Navbar from "./component/Navbar";
-import Calendar from "./component/Calendar";
+import Navbar from "./components/Navbar";
+import Calendar from "./components/Calendar";
 
 function App() {
   return (
