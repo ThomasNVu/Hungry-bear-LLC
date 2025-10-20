e616238d08046e639194b49b37fd9b109a804ba5
ThomasNVu <139417064+ThomasNVu@users.noreply.github.com>
2025-10-19 20:07:45 -0400

    Create return page for broken links

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,11 +5,13 @@ import Login from "./Login.tsx";
 import SignUp from "./SignUp.tsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import PageNotFound from "./Components/PageNotFound.tsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/Login", element: <Login /> },
   { path: "/SignUp", element: <SignUp /> },
+  { path: "*", element: <PageNotFound /> },
 ]);
 
 createRoot(document.getElementById("root")!).render(
