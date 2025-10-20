704f29f62452c0c12f898a1a52ac1d1e696df484
ThomasNVu <139417064+ThomasNVu@users.noreply.github.com>
2025-10-19 20:03:00 -0400

    Add React Router to main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import Login from "./Login.tsx";
+import SignUp from "./SignUp.tsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/Login", element: <Login /> },
+  { path: "/SignUp", element: <SignUp /> },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
